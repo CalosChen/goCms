f4fac818d140b781ddb71ab9bb1bede25e41f32f
Caloch <359000081@qq.com>
2021-06-07 13:44:42 +0800

    change format to use printf for printing out port;

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"goCms/clients"
+	"goCms/models"
 	"goCms/services"
 	"net/http"
 )
@@ -11,14 +12,19 @@ func main() {
 
 	service := services.Service{}
 	service.GetConfig()
-	mysql := clients.MysqlClient{}
-	mysql.Connect()
+	// mysql := clients.MysqlClient{}
+	// mysql.Connect()
 
 	fmt.Println("Welcome to use the simple golang CMS system.")
 	http.HandleFunc("/health", func(w http.ResponseWriter, req *http.Request) {
+		respondent := models.Respondent{ID: 1, Name: "hello world."}
 		w.Write([]byte("hello"))
+		data, err := json.Marshal(respondent)
+		if err == nil {
+			w.Write(data)
+		}
 	})
-	fmt.Println("starting server...")
+	fmt.Printf("starting server on port:%d...", 9999)
 	http.ListenAndServe("127.0.0.1:9999", nil)
 
 }
