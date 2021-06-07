878d6c7a5ff1d41ecf730f8097e30e2f94a86b58
Caloch <359000081@qq.com>
2021-06-07 14:12:25 +0800

    add cache default constants for duration and namespace;

--- services/cache.go
+++ services/cache.go
@@ -1,1 +1,14 @@
 package services
+
+import (
+	"fmt"
+)
+
+const (
+	defaultCachedDuration  = 5000
+	defaultCachedNamespace = "goCms."
+)
+
+func Store(key string, obj interface{}) {
+	fmt.Println("defs:", defaultCachedDuration, defaultCachedNamespace)
+}
